908bfa7a3ee19b298ce0e55d5154ab4646e228d5
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-05-08 21:29:53 +0200

    Add conditional statement for all possible outcomes of the game

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ console.log('Gracz wpisał: ' + playerInput);
 let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
-  playerMove = 'kamień';
+  	playerMove = 'kamień';
 }
 else if(playerInput == '2') {
 	playerMove = 'papier';
@@ -33,3 +33,40 @@ else if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+	printMessage('Komputer wygrywa!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
+	printMessage('Komputer wygrywa!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage('Komputer wygrywa!');
+}
+else if( computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Mamy remis!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+	printMessage('Mamy remis!');
+}
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
+	printMessage('Mamy remis!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nieznany ruch') {
+	printMessage('Powtórz ruch gracza');
+}
+else if( computerMove == 'papier' && playerMove == 'nieznany ruch') {
+	printMessage('Powtórz ruch gracza');
+}
+else if( computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
+	printMessage('Powtórz ruch gracza');
+}
